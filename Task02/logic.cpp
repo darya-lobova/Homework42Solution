cabc54e822fe39252e7a53a889566cc537769b8c
darya-loboca <dasha.lobova.it@gmail.com>
2026-03-01 17:49:24 +0300

    task02 solved

--- Task02/logic.cpp
+++ Task02/logic.cpp
@@ -4,18 +4,18 @@ bool is_vector_ordered(int array[DEFAULT_SIZE], int length) {
 	bool is_ascending = true;
 	bool is_descending = true;
 
-    for (size_t i = 1; i < length; ++i) {
-        if (array[i] < array[i - 1]) {
-            is_ascending = false;  
-        }
-        if (array[i] > array[i - 1]) {
-            is_descending = false; 
-        }
-    }
-    return is_ascending || is_descending;
+	for (size_t i = 1; i < length; ++i) {
+		if (array[i] < array[i - 1]) {
+			is_ascending = false;
+		}
+		if (array[i] > array[i - 1]) {
+			is_descending = false;
+		}
+	}
+	return is_ascending || is_descending;
 
 }
 
-	void print(string s) {
-		cout << s;
-	}
+void print(string s) {
+	cout << s;
+}
